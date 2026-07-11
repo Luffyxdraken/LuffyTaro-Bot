032eeee1e4e88f9c3a21a3a5347dd4552b9bf997
Luffyxdraken <129548782+Luffyxdraken@users.noreply.github.com>
2026-07-12 00:04:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv';
+import path from 'path';
 dotenv.config();
 
 export const CONFIG = {
   SESSION_ID: process.env.SESSION_ID || '',
-  SESSION_DIR: '/data/session',
-  DATABASE_PATH: '/data/database.sqlite',
+  SESSION_DIR: path.join(process.cwd(), 'data', 'session'),
+  DATABASE_PATH: path.join(process.cwd(), 'data', 'database.sqlite'),
   PREFIX: process.env.PREFIX || '.',
   OWNER: (process.env.OWNER_NUMBER || '917866052212') + '@s.whatsapp.net'
 };
