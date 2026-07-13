1862736a01500179a0fe2751d665a6caffad95b6
Luffyxdraken <129548782+Luffyxdraken@users.noreply.github.com>
2026-07-14 01:33:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,12 @@ export const CONFIG = {
   SESSION_ID: process.env.SESSION_ID || '',
   SESSION_DIR: path.join(process.cwd(), 'data', 'session'),
   DATABASE_PATH: path.join(process.cwd(), 'data', 'database.sqlite'),
-  PREFIX: process.env.PREFIX || '.',
-  OWNER: (process.env.OWNER_NUMBER || '917866052212') + '@s.whatsapp.net'
+  PREFIX: '.',
+  
+  // 👑 MASTER OWNER ACCOUNT
+  OWNER: '917866052212@s.whatsapp.net',
+
+  // 🔗 CODES & STRINGS
+  MAIN_GROUP_JID: '120363198374920134@g.us', 
+  MAIN_GROUP_INVITE_LINK: 'https://chat.whatsapp.com/FzIxR7J8W1W8BB0z90ectp'
 };
